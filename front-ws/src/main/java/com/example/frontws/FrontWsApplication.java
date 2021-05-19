7e75d347e3aca4410341af967b66bfd0ec51cd88
seongbin <kled@kakao.com>
2021-05-19 20:51:54 +0900

    fix .

---
+++ front-ws/src/main/java/com/example/frontws/FrontWsApplication.java
@@ -0,0 +1,13 @@
+package com.example.frontws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontWsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrontWsApplication.class, args);
+    }
+
+}
---
+++ front-ws/src/test/java/com/example/frontws/FrontWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.frontws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontWsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
